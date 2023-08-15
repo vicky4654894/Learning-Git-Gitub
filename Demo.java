5737a7ed2a41bc51440f561b64cb7499d0892e9d
Vicky Kumar <vickysingh.grd1211@gmail.com>
2023-08-15 14:17:49 +0530

    add changes in Demo

--- Demo.java
+++ Demo.java
@@ -2,6 +2,6 @@ import java.util.Scanner;
 class Demo{
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-        System.out.println("Hello");
+        System.out.println("Hello World");
     }
 }
